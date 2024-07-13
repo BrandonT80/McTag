f361c1aada28c1bdd8527e1d985992424c7dbc32
BrandonT80 <31973893+BrandonT80@users.noreply.github.com>
2024-07-13 17:25:57 -0500

    Updates to 1.20.1 and adds Scoreboards
    Updates to 1.20.1 and adds Scoreboards
    Also bugfixes

--- src/main/java/bct/loadupstudios/MCTag/Scoreboard.java
+++ src/main/java/bct/loadupstudios/MCTag/Scoreboard.java
@@ -1,8 +1,9 @@
 package bct.loadupstudios.MCTag;
 import fr.mrmicky.fastboard.FastBoard;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 
-import java.util.List;
 import java.util.UUID;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,66 +14,74 @@ public class Scoreboard {
     private final Map<UUID, FastBoard> scoreboards = new HashMap<>();    //Player Scoreboards
     private Map<String, Integer> playerScores = new HashMap<String, Integer>();
     private Logger logger;
-    Scoreboard(Logger logger)
-    {
+    private Tag tagClass;
+    private FileConfiguration config;
+
+    Scoreboard(Logger logger, Tag tagClass, FileConfiguration c) {
         this.logger = logger;
+        this.tagClass = tagClass;
+        this.config = c;
     }
 
     public boolean addPlayerScoreboard(String playerName) {
         try {
             FastBoard board = new FastBoard(Bukkit.getPlayer(playerName));
             scoreboards.put(Bukkit.getPlayer(playerName).getUniqueId(), board);
+            updateScoreboard();
         }
-        catch(Exception e)
-        {
+        catch(Exception e) {
             logger.log(Level.INFO, "Failed to add player scoreboard: " + e);
             return false;
         }
         return true;
     }
 
-    public boolean addScore(String playerName)
-    {
+    public boolean removePlayerScoreboard(String playerName) {
+        try {
+            scoreboards.get(Bukkit.getPlayer(playerName).getUniqueId()).delete();   //Deletes the scoreboard from the player
+            scoreboards.remove(Bukkit.getPlayer(playerName).getUniqueId());         //Removes the now deleted scoreboard from the list
+        }
+        catch(Exception e) {
+            logger.log(Level.INFO, "Failed to remove player scoreboard: " + e);
+            return false;
+        }
+        return true;
+    }
+
+
+    public boolean addScore(String playerName) {
         //Add score locally to update the scoreboards, every 25 or so tags force update the database
         //And clear this local input
         return true;
     }
 
-    public boolean updateDatabaseScores()
-    {
+    public boolean updateDatabaseScores() {
         //Logic to take all current database additions and update the table
         return true;
     }
 
-    public boolean pullScoreboards()
-    {
+    public boolean pullScoreboards() {
         //Pull all players from database
         //Sort players from highest to lowest tag counts
-
-        /*Fable Code
-        for (FastBoard board : scoreboards.values()) {
-            if (board.getLines().size() > (players.size() + 1)) {
-                for (FastBoard boardz : scoreboards.values()) {
-                    try {
-                        if (!board.isDeleted()) {
-                            boardz.updateLines("");
-                            boardz.updateLine(0, "");
-                        }
-                    } catch (Exception e) {
-                        logger.log(Level.WARNING, "Error updating fastboard lines: " + e);
-                    }
-                }
-
-            }
-            for (int i = 0; i < players.size(); i++) {
-                try {
-                    board.updateLine(i + 1, "" + players.get(i).getPlayerName() + ": " + players.get(i).getPoints());
-                } catch (Exception e) {
-                    logger.log(Level.WARNING, "Error updating fastboard: " + e);
-                }
-            }
-        }*/
-
         return true;
+    }
+
+    public void updateScoreboard() {
+        //For each board
+        for (FastBoard board : scoreboards.values()) {
+            try {
+                if (!board.isDeleted()) {
+                    board.updateLines("");
+                    board.updateTitle(ChatColor.translateAlternateColorCodes('&',"&cTagged Player:"));
+                    board.updateLine(0, tagClass.getCurrentTag());
+                }
+            } catch (Exception e) {
+                logger.log(Level.WARNING, "Error updating fastboard lines: " + e);
+            }
+        }
+    }
+
+    public boolean isScoreboardsActive() {
+        return config.getBoolean("scoreboards");
     }
 }
--- src/main/java/bct/loadupstudios/MCTag/Tag.java
+++ src/main/java/bct/loadupstudios/MCTag/Tag.java
@@ -1,11 +1,14 @@
 package bct.loadupstudios.MCTag;
 
+import com.google.common.util.concurrent.ServiceManager;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,73 +21,55 @@ public class Tag
 	ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
 	
 	Logger logger;
+	MyListener eventManager;
 	
 	Tag(FileConfiguration c, Logger l)
 	{
 		config = c;
 		logger = l;
-		if(!getConfig().getString("taggedPlayer").equals("noPlayer"))
-		{
+		if(!getConfig().getString("taggedPlayer").equals("noPlayer")) {
 			taggedPlayer = getConfig().getString("taggedPlayer");
 		}
-		else
-		{
+		else {
 			taggedPlayer = null;
 		}
 	}
-	
-	public void reload(FileConfiguration conf)
+
+	public void setEventManager(MyListener eventManager)
 	{
+		this.eventManager = eventManager;
+	}
+	
+	public void reload(FileConfiguration conf) {
 		config = conf;
-		System.out.println("T: " + config.getString("taggedPlayer"));
 		if(taggedPlayer != config.getString("taggedPlayer"))
 		{
-			if(config.getString("taggedPlayer") == "noPlayer")
-			{
-			
-			}
-			else
-			{
+			if(!config.getString("taggedPlayer").equals("noPlayer")) {
 				tagPlayer(config.getString("taggedPlayer"), taggedPlayer);
 			}
 		}
 	}
 	
 	
-	public void tagPlayer(String newtagplayer, String oldtagplayer)
-	{
-		ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
-		taggedPlayer = newtagplayer;
-		String command = "nick " + oldtagplayer + " off";
-		Bukkit.dispatchCommand(console, command);
-		command = "nick " + newtagplayer + " &c" + newtagplayer;
-		Bukkit.dispatchCommand(console, command);
-		//command = "tag";
-		//Bukkit.dispatchCommand(console, command);
+	public void tagPlayer(String newtagplayer, String oldtagplayer) {
 		notifyPlayer(newtagplayer, oldtagplayer);
-		
+		taggedPlayer = newtagplayer;
+		eventManager.scoreboards.updateScoreboard();
 		Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',"&c" + newtagplayer + " &bhas been tagged!"));
 	}
 	
-	public void tagPlayer(String newtagplayer)
-	{
-		if(taggedPlayer == null)
-		{
+	public void tagPlayer(String newtagplayer) {
+		if(taggedPlayer == null) {
 			taggedPlayer = newtagplayer;
-			String command = "nick " + newtagplayer + " &c" + newtagplayer;
-			//Bukkit.dispatchCommand(console, command);
-			//command = "tag";
-			Bukkit.dispatchCommand(console, command);
 			notifyPlayer(newtagplayer, null);
+			eventManager.scoreboards.updateScoreboard();
 			Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',"&c" + newtagplayer + " &bhas been tagged!"));
 		}
-		else if(!taggedPlayer.equals(newtagplayer))
-		{
+		else if(!taggedPlayer.equals(newtagplayer)) {
 			tagPlayer(newtagplayer, taggedPlayer);
+			eventManager.scoreboards.updateScoreboard();
 		}
-		else if(taggedPlayer.equals(newtagplayer))
-		{
-			//System.out.println("Random tagged player is already tagged");
+		else if(taggedPlayer.equals(newtagplayer)) {
 			logger.log(Level.INFO, "Random tagged player is already tagged");
 			Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',"&c" + newtagplayer + " &bhas been tagged by the GODS, again!"));
 		}
@@ -100,72 +85,57 @@ public class Tag
 		return config;
 	}
 	
-	private void notifyPlayer(String tagPlayerName, String untagPlayerName)
-	{
-		//System.out.println("Notify: " + playerName);
+	private void notifyPlayer(String tagPlayerName, String untagPlayerName) {
 		Player tagPlayer = Bukkit.getPlayer(tagPlayerName);
 		Player untagPlayer = null;
-		if(untagPlayerName != null)
-		{
+		if(untagPlayerName != null) {
 			untagPlayer = Bukkit.getPlayer(untagPlayerName);
 		}
-		//System.out.println("Notify2: " + player);
-		if(untagPlayer != null)
-		{
-			if(tagPlayer != null)
-			{
-				tagPlayer.sendMessage("You have been tagged!");
+		if(untagPlayer != null) {
+			if(tagPlayer != null) {
+				tagPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', "&2You have been tagged!"));
 			}
-			untagPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&',"You have tagged &c" + tagPlayerName));
+			untagPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&',"&bYou have tagged &c" + tagPlayerName));
 		}	
-		else
-		{
-			if(tagPlayer != null)
-			{
-				tagPlayer.sendMessage("You have been tagged!");
+		else {
+			if(tagPlayer != null) {
+				tagPlayer.sendMessage(ChatColor.translateAlternateColorCodes('&', "&2You have been tagged!"));
 			}
 		}
 	}
 	
-	public void it(CommandSender sender)
-	{
-		sender.sendMessage("Current tagged player: " + getCurrentTag());
+	public void it(CommandSender sender) {
+		sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&bCurrent tagged player: &2" + getCurrentTag()));
 	}
 	
-	public void start(CommandSender sender)
-	{
-		//System.out.println("Start Called");
+	public void start(CommandSender sender, String removedPlayer) {
 		Object[] players = Bukkit.getOnlinePlayers().toArray();
-		for(int i = 0; i > -1; i++)
-		{
+		for(int i = 0; i > -1; i++) {
 			Object name = players[rand.nextInt(Bukkit.getOnlinePlayers().size())];
 			String playerName = name.toString().substring(17, name.toString().length()-1);
-			//System.out.println("Name: " + playerName);
-			//System.out.println("Start End");
-			if(Bukkit.getPlayer(playerName).hasPermission("mctag.opt"))
-			{
+			if(!playerName.equals(removedPlayer) && eventManager.readPlayer(Bukkit.getPlayer(playerName).getName()) == 1) {
 				tagPlayer(playerName);
+				eventManager.scoreboards.updateScoreboard();
 				break;
 			}
-			else
-			{ 
-				if(i != 20)
-				{
-					//System.out.println("Player " + playerName + " is opted out of tag, finding another player...");
+			else {
+				if(i != 20) {
 					logger.log(Level.WARNING, "Player " + playerName + " is opted out of tag, finding another player...");
 				} 
-				else 
-				{
-					//System.out.println("Failed to find a suitable player in 20 iterations, this is likely due to many, or all players opting out. You may try again or specify a player using /tag <playername>");
+				else {
 					logger.log(Level.WARNING, "Failed to find a suitable player in 20 iterations, this is likely due to many, or all players opting out. You may try again or specify a player using /mctag <playername>");
-					if(sender != null)
-					{
-						sender.sendMessage("Failed to find a suitable player, please try again. Read console for more information.");
+					if(sender != null) {
+						sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cFailed to find a suitable player, please try again. Read console for more information."));
 					}
 					break;
 				}
 			}
 		}
 	}
-	
+
+	public void restartTagIfRemovedPlayerIsTagged(String checkPlayer) {
+		if(getCurrentTag().equals(checkPlayer)) {                       //Restarts the tag if the player removed is the tagged player (Either logout or optout)
+			start(null, checkPlayer);
+		}
+	}
 }
